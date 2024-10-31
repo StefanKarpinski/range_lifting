4ddd43c5d30455582f61b71e8bbfa1035874c281
Stefan Karpinski <stefan@karpinski.org>
2024-10-31 16:01:35 -0400

    revive the p-scanning approach and finish it

--- work.jl
+++ work.jl
@@ -339,6 +339,65 @@ function lclc(a1::Int, b1::Int, a2::Int, b2::Int)
     return n
 end
 
+# least common linear combination, i.e. smallest positive n such that
+#
+#   n == a*i + b*j == c*k + d*l
+#
+# for nonnegative i, j, k, l
+#
+function lclc(a::Int, b::Int, c::Int, d::Int)
+    @show a, b, c, d
+
+    g_ac, u_ac = gcdx(a, c)
+    g_ad, u_ad = gcdx(a, d)
+    g_bc, u_bc = gcdx(b, c)
+    g_bd, u_bd = gcdx(b, d)
+
+    if g_ac*g_bd < g_ad*g_bc
+        c, d = d, c
+        g_ac, g_ad = g_ad, g_ac
+        g_bc, g_bd = g_bd, g_bc
+        u_ac, u_ad = u_ad, u_ac
+        u_bc, u_bd = u_bd, u_bc
+    end
+    g = g_ac*g_bd
+
+    p_max(n) = max(
+        min(a*fld(n-1,a), d*fld(n-1,d)),
+        min(b*fld(n-1,b), c*fld(n-1,c)),
+    )
+    n = min(a*c÷g_ac, b*d÷g_bd) # n for p = 0
+    P = p_max(n)
+    @show n, P
+    p = 0
+    while (p += g) ≤ P
+        for p in (-p, p)
+            i = mod(+p*u_ac, c) ÷ g_ac
+            k = (a*i - p) ÷ c
+            while k < 0
+                i += a*c÷g_ac
+                k = (a*i - p) ÷ c
+            end
+            j = mod(-p*u_bd, d) ÷ g_bd
+            l = (b*j + p) ÷ d
+            while l < 0
+                j += b*d÷g_bd
+                l = (b*j + p) ÷ d
+            end
+            @assert a*i + b*j == c*k + d*l
+            n′ = a*i + b*j
+            println(lpad(p, 4), ": ", n′)
+            if n′ < n
+                n = n′
+                P = p_max(n)
+                @show n, P
+            end
+        end
+    end
+
+    return @show n
+end
+
 #=
 for _ = 1:100000
     a, b, c, d = rand(1:100, 4)
